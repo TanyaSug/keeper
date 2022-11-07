92b9bdbabca59ed8abb75c1dfb40f082db166c6f
Tanya Suglobova <tany.suglobova@gmail.com>
2022-11-07 14:57:35 +1300

    created authentication page component alongside sign in form

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -10,10 +10,10 @@ const Navigation = () => {
                 </Link>
 
                 <div className='nav-links-container'>
-                    <Link className='nav-link' to='/sign-in'>
-                        SIGN IN
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
                     </Link>
-                    <Link className='nav-link' to='/sign-in'>
+                    <Link className='nav-link' to='/auth'>
                         SIGN IN
                     </Link>
                 </div>
